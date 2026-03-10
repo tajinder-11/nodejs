b586ddc6bf56dd7fbf46977268c78ec7d107b094
tajinder-06 <157876126+tajinder-06@users.noreply.github.com>
2026-03-10 17:10:21 +0530

    chore: changed id to uuid

--- Authentication_session/models/userModel.js
+++ Authentication_session/models/userModel.js
@@ -1,6 +1,6 @@
-import { integer, pgTable, varchar } from "drizzle-orm/pg-core";
+import { integer, pgTable, varchar, uuid } from "drizzle-orm/pg-core";
 export const usersTable = pgTable("users", {
-  id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  id: uuid().primaryKey().generatedAlwaysAsIdentity(),
   name: varchar({ length: 255 }).notNull(),
   age: integer().notNull(),
   email: varchar({ length: 255 }).notNull().unique(),
