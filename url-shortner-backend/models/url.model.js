a976fe1de96721b6c38d93c24bcd919eb6b13fc4
tajinder-11 <dheertajinder@gmail.com>
2026-05-09 21:52:34 +0530

    chore: created urlsTable schema

---
+++ url-shortner-backend/models/url.model.js
@@ -0,0 +1,19 @@
+import { pgTable, timestamp, uuid, varchar, text } from 'drizzle-orm/pg-core';
+import { usersTable } from './user.model.js';
+
+export const urlsTable = pgTable('urls', {
+  id: uuid().primaryKey().defaultRandom(),
+
+  shortCode: varchar('code', { length: 155 }).notNull().unique(),
+  targetURL: text('target_url').notNull(),
+
+  userId: uuid('user_id')
+    .references(() => usersTable.id)
+    .notNull(),
+
+  createdAt: timestamp('created_at').defaultNow().notNull(),
+  updatedAt: timestamp('updated_at')
+    .defaultNow()
+    .$onUpdate(() => new Date())
+    .notNull(),
+});
