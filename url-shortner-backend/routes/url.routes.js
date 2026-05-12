2833064728f7df62bf634661c5b47608a009667e
tajinder-11 <dheertajinder@gmail.com>
2026-05-12 23:30:56 +0530

    chore: created delete url api

--- url-shortner-backend/routes/url.routes.js
+++ url-shortner-backend/routes/url.routes.js
@@ -2,9 +2,10 @@ import express from 'express';
 import { shortenPostRequestBodySchema } from '../validations/request.validation.js';
 import { nanoid } from 'nanoid';
 import { db } from '../db/index.js';
-import { urlsTable } from '../models/index.js';
+import { urlsTable, usersTable } from '../models/index.js';
 import { ensureAuthenticated } from '../middlewares/auth.middleware.js';
 import { createNewUrl } from '../services/urls.services.js';
+import { eq, and } from 'drizzle-orm';
 
 const router = express.Router();
 
@@ -31,5 +32,40 @@ router.post('/shorten', ensureAuthenticated, async (req, res) => {
     targetURL: result.targetURL,
   });
 });
+
+router.get('/codes', ensureAuthenticated, async function (req, res) {
+  const codes = await db
+    .select()
+    .from(urlsTable)
+    .where(eq(urlsTable.userId, req.user.id));
+
+  return res.status(200).json({ codes });
+});
+
+router.delete('/:id', ensureAuthenticated, async function (req, res) {
+  const id = req.params.id;
+  await db
+    .delete(urlsTable)
+    .where(and(eq(urlsTable.id, id), eq(urlsTable.userId, req.user.id)));
+  res.status(200).json({ deleted: true });
+});
+
+router.get('/:shortCode', async function (req, res) {
+  const code = req.params.shortCode;
+  const [result] = await db
+    .select({
+      targetUrl: urlsTable.targetURL,
+    })
+    .from(urlsTable)
+    .where(eq(urlsTable.shortCode, code));
+
+  if (!result) {
+    return res.status(404).json({ error: 'Invalid url' });
+  }
+
+  return res.redirect(result.targetUrl);
+});
+
+// updateUrl api
 
 export default router;
